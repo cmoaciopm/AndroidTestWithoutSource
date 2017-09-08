66684ff74c78e617acca56aa8c64f7473dc9116f
Anmin Gong <agong@Anmins-MacBook-Pro.local>
2017-09-09 00:24:12 +0800

    1.Demonstratr how to test app "KeyEvent Display" 2.Add customized KeyEventAction to perform down or up keyevent seperately

--- src/main/java/net/cmoaciopm/androidtest/TestWithoutSourceCode.java
+++ src/main/java/net/cmoaciopm/androidtest/TestWithoutSourceCode.java
@@ -1,9 +1,13 @@
 package net.cmoaciopm.androidtest;
 
 import android.app.Instrumentation;
+import android.content.Context;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.KeyEvent;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,13 +16,17 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static net.cmoaciopm.androidtest.MyKeyEventActions.keyDown;
+import static net.cmoaciopm.androidtest.MyKeyEventActions.keyUp;
+import static org.hamcrest.CoreMatchers.containsString;
 
 
 @RunWith(AndroidJUnit4.class)
 public class TestWithoutSourceCode {
 
-    private static final String ACTIVITY_TO_BE_TESTED = "com.example.android.apis.ApiDemos";
+    private static final String ACTIVITY_TO_BE_TESTED = "aws.apps.keyeventdisplay.MainActivity";
 
     @Before
     public void setup() {
@@ -44,7 +52,26 @@ public class TestWithoutSourceCode {
 
     @Test
     public void test() {
-        onView(withText("API Demos")).check(matches(isDisplayed()));
+        onView(withText("Clear")).check(matches(isDisplayed()));
+        ViewInteraction fldEventTextView = onView(withId(getId("fldEvent")));
+        fldEventTextView.check(matches(isDisplayed()));
+        Espresso.pressBack();
+        fldEventTextView.check(matches(withText(containsString("code=" + KeyEvent.KEYCODE_BACK))));
+
+        fldEventTextView.perform(keyDown(KeyEvent.KEYCODE_CTRL_LEFT));
+        fldEventTextView.check(matches(withText(containsString("action=0 code=" + KeyEvent.KEYCODE_CTRL_LEFT))));
+        fldEventTextView.perform(keyDown(KeyEvent.KEYCODE_C));
+        fldEventTextView.check(matches(withText(containsString("action=0 code=" + KeyEvent.KEYCODE_C))));
+        fldEventTextView.perform(keyUp(KeyEvent.KEYCODE_C));
+        fldEventTextView.check(matches(withText(containsString("action=1 code=" + KeyEvent.KEYCODE_C))));
+        fldEventTextView.perform(keyUp(KeyEvent.KEYCODE_CTRL_LEFT));
+        fldEventTextView.check(matches(withText(containsString("action=1 code=" + KeyEvent.KEYCODE_CTRL_LEFT))));
+    }
+
+    public static int getId(String id) {
+        Context targetContext = InstrumentationRegistry.getTargetContext();
+        String packageName = targetContext.getPackageName();
+        return targetContext.getResources().getIdentifier(id, "id", packageName);
     }
 }
 
